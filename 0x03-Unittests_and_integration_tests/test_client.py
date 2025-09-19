7e5cf4c4766ab61e1acaf293b76a224284a3aac2
Authcornelius <corneliusedos@gmail.com>
2025-09-19 16:41:14 +0100

    be task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,28 +3,25 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test GithubOrgClient class"""
 
+    @patch("client.get_json")  # patch first
     @parameterized.expand([
         ("google",),
         ("abc",)
     ])
-    @patch("client.get_json")  # patch get_json in the client module
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, mock_get_json, org_name):
         """Test that GithubOrgClient.org returns the expected value"""
-        # Set what the mocked get_json should return
         expected_payload = {"login": org_name}
         mock_get_json.return_value = expected_payload
 
         client = GithubOrgClient(org_name)
         result = client.org
 
-        # Assert get_json was called once with the right URL
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
-        # Assert that the result is what we mocked
         self.assertEqual(result, expected_payload)
