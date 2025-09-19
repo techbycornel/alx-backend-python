dab6cb5f15828c304e702343cbee7142960e6c99
Authcornelius <corneliusedos@gmail.com>
2025-09-19 15:28:19 +0100

    be task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ Unit tests for utils functions
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -53,6 +53,37 @@ class TestGetJson(unittest.TestCase):
         # Assertions
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for utils.memoize decorator"""
+
+    def test_memoize(self):
+        """Test memoize caches the result of a method"""
+
+        class TestClass:
+            """Test class for memoization"""
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        obj = TestClass()
+
+        with patch.object(obj, "a_method", return_value=42) as mock_method:
+            # Call a_property twice
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            # Check result is correct
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            # a_method should only be called once
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
