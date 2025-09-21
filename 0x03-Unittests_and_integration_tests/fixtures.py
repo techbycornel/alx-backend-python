f28686ddb563e6bab55c8a462b15044ce948074a
Authcornelius <corneliusedos@gmail.com>
2025-09-21 19:08:13 +0100

    updated

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,7 +1,7 @@
 # fixtures.py
 
 org_payload = {
-    "login": "my_org",
+    "login": "test_org",
     "id": 1,
     "node_id": "MDQ6VXNlcjE=",
     "url": "https://api.github.com/orgs/my_org",
@@ -16,10 +16,11 @@ org_payload = {
 }
 
 repos_payload = [
-    {"id": 123, "name": "repo1", "license": {"key": "mit"}},
-    {"id": 456, "name": "repo2", "license": {"key": "apache-2.0"}},
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+    {"name": "repo3", "license": {"key": None}},
 ]
 
-expected_repos = ["repo1", "repo2"]
+expected_repos = ["repo1", "repo2", "repo3"]
 
 apache2_repos = ["repo1"]
