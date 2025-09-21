37fb87d9c042114816e01a79b04b0b0469f9c629
Authcornelius <corneliusedos@gmail.com>
2025-09-21 19:12:06 +0100

    updated

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,15 +1,13 @@
-# fixtures.py
+#!/usr/bin/env python3
+"""
+Fixtures for unit and integration tests
+"""
 
-org_payload = {
-    "login": "test_org",
-}
-
+org_payload = {"login": "test_org"}
 repos_payload = [
     {"name": "repo1", "license": {"key": "apache-2.0"}},
     {"name": "repo2", "license": {"key": "mit"}},
     {"name": "repo3", "license": {"key": None}},
 ]
-
 expected_repos = ["repo1", "repo2", "repo3"]
-
 apache2_repos = ["repo1"]
