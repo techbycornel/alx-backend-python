fd264e4873f242f64965e869fb3ed7c36e5ce01b
Authcornelius <corneliusedos@gmail.com>
2025-09-26 16:15:57 +0100

    updated

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,6 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
 
 class IsParticipantOfConversation(BasePermission):
     """
@@ -8,7 +10,7 @@ class IsParticipantOfConversation(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # For messages, check if user is sender or in conversation participants
-        if hasattr(obj, "conversation"):  
+        if hasattr(obj, "conversation"):
             return request.user in obj.conversation.participants.all()
 
         # For conversations, check if user is a participant
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from rest_framework import permissions
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
