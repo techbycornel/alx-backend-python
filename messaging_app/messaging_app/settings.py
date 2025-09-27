97931834ebb654db7da0e89c3cb493f7d8043357
Authcornelius <corneliusedos@gmail.com>
2025-09-27 10:17:26 +0100

    updated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +45,17 @@ INSTALLED_APPS = [
     'chats',
     'django_filters',
 ]
+
+# JWT Configuration
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # adjust if needed
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "USER_ID_FIELD": "user_id",        # <--- matches your custom User PK
+    "USER_ID_CLAIM": "user_id",        # token claim name
+}
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
