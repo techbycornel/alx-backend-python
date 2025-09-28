8fb1854ea2285b756f021ad14781fb8ef5a22d03
Authcornelius <corneliusedos@gmail.com>
2025-09-28 13:07:47 +0100

    updated

--- Django-Middleware-0x03/0x03-MessagingApp-Django/asgi.py
+++ Django-Middleware-0x03/0x03-MessagingApp-Django/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-Middleware-0x03.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '0x03-MessagingApp-Django.settings')
 
 application = get_asgi_application()
